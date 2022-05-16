ff43b42afb4eb72ff8e2fef4a3f505c504ddeb14
andruhovski <andruhovski@gmail.com>
2022-05-16 23:35:06 +0300

    Update dir

--- asposepdf/__init__.py
+++ asposepdf/__init__.py
@@ -2,6 +2,6 @@ import jpype
 import os
 
 __asposepdf_dir__ = os.path.dirname(__file__)
-__asposepdf_jar_path__ = __asposepdf_dir__ + "/javalib/aspose-barcode-python-22.4.jar"
+__asposepdf_jar_path__ = __asposepdf_dir__ + "/javalib/aspose.pdf-22.4.jar"
 jpype.addClassPath("__asposepdf_jar_path__")
 jpype.startJVM(jpype.getDefaultJVMPath())
